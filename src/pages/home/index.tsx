3c5750e85985fb33ea05ffc793032c42bf9df7c6
Matheus Messias <matheus.jf@live.com>
2023-07-25 10:49:49 -0300

    change database url to deploy db in planetscale

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,27 +4,34 @@ import Image from 'next/image'
 
 import previewImage from '../../assets/app-preview.png'
 import { ClaimUsernamForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento Descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernamForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          alt="Calendario simbolizando aplicação em funcionamento"
-          height={400} // altura máxima que essa imagem pode assumir
-          quality={100} // por padrão vem setada como 80% de qualidade das imagens usando Image do Next
-          priority // por ser uma imagem principal na home, não deixa o carregamento dela por último
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title='Descomplique sua agenda | Ignite Call'
+        description='Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.'
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento Descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernamForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            alt="Calendario simbolizando aplicação em funcionamento"
+            height={400} // altura máxima que essa imagem pode assumir
+            quality={100} // por padrão vem setada como 80% de qualidade das imagens usando Image do Next
+            priority // por ser uma imagem principal na home, não deixa o carregamento dela por último
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
